b5da81625523335a3e7222de6770fbe051b35816
Ashutosh Shioorkar <shioorkarashutosh@gmail.com>
2026-06-27 08:27:01 +0530

    Refactor app.py to clean up commented code

--- app.py
+++ app.py
@@ -7,26 +7,10 @@ import time
 app = Flask(__name__)
 app.secret_key = 'something'
 
-#https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple
-
-# {'response_code': 0, 'results': [{'type': 'multiple', 'difficulty': 'easy', 'category': 'General Knowledge', 'question': 'What geometric shape is generally used for stop signs?', 'correct_answer': 'Octagon', 'incorrect_answers': ['Hexagon', 'Circle', 'Triangle']}, 
-# {'type': 'multiple', 'difficulty': 'easy', 'category': 'General Knowledge', 'question': 'Which one of the following rhythm games was made by Harmonix?', 'correct_answer': 'Rock Band', 'incorrect_answers': ['Meat Beat Mania', 'Guitar Hero Live', 'Dance Dance Revolution']}
-
-#for question
-# questions['results'[current_question]['question']]    
-#for answer
-#student_answer = questions['results'][current_question]['correct_answer']
-
-
-
-
-
 @app.route('/', methods = ['GET','POST'])
 def index():
     if request.method == 'POST':
         no_ques = request.form.get('no_ques')
-        category_api = requests.get(f'https://opentdb.com/api_category.php')
-        category_list = category_api.json()
         category = request.form.get('category')
         diff = request.form.get('diff')
         type = request.form.get('type')
@@ -39,69 +23,6 @@ def index():
 
         return redirect(url_for('quiz'))
     return render_template('index.html')
-
-
-# @app.route('/quiz', methods = ['GET','POST'])
-# def quiz():    
-
-#     question_no = len( session['questions']['results'] )
-
-#     if session['current_question'] < question_no:
-
-#         current = session['questions']['results'][session['current_question']]
-
-#         questions = current['question']
-
-#         correct_answer = current['correct_answer']
-
-#         options = current['incorrect_answers'].copy()
-#         options.append(correct_answer)
-#         r.shuffle(options)
-#     #     incorrect_answers = session['questions']['results'][session['current_question']]['incorrect_answers']
-#     #     correct_answer = session['questions']['results'][session['current_question']]['correct_answer']
-#     #     if correct_answer not in incorrect_answers:
-#     #         incorrect_answers.append(correct_answer)
-#     #     r.shuffle(incorrect_answers)
-#     #     options = incorrect_answers
-
-#     #     #SHOWING QUESTIONS
-#     #     questions = session['questions']['results'][session['current_question']]['question']
-
-    
-#     else :
-#         return redirect(url_for('result'))
-    
-#     # ACCEPTING ANSWERS AND CHECKING 
-#     if request.method == 'POST':
-#         answer = request.form.get('answer')
-#         if answer ==  correct_answer:
-#             session['score'] += 1
-
-#         session['current_question'] += 1
-
-#         if session['current_question'] >= question_no:
-#             questions = session['questions']['results'][session['current_question']]['question']
-#             return redirect(url_for('result'))
-
-#     # if request.method == "POST":
-#     #     print("POST BLOCK EXECUTED")
-
-#     #     answer = request.form.get("answer")
-
-#     #     if answer == correct_answer:
-#     #         session["score"] += 1
-
-#     #     session["current_question"] += 1
-
-#     #     print("After Increment:", session["current_question"])
-
-#     # print("----------------------")
-#     # print("Method:", request.method)
-#     # print("Current:", session["current_question"])
-#     # print("Total:", question_no)
-
-
-#     return render_template('quiz.html', question = questions, options = options)
 
 
 @app.route('/quiz', methods=['GET', 'POST'])
